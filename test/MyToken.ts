8070a64f7163e9ac1de9c32640d87eda7380c2cf
awj1052 <awj1052@naver.com>
2025-04-02 00:59:11 +0900

    test: totalSupply, balanceOf

--- test/MyToken.ts
+++ test/MyToken.ts
@@ -1,10 +1,14 @@
 import hre from "hardhat";
 import { expect } from "chai";
+import { MyToken } from "../typechain-types";
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
 describe("MyToken", () => {
-    let myTokenC: any;
+    let myTokenC: MyToken;
+    let signers: HardhatEthersSigner[];
 
     before(async () => {
+        signers = await hre.ethers.getSigners();
         myTokenC = await hre.ethers.deployContract("MyToken", ["MyToken", "MT", 18]);
     });
 
@@ -18,5 +22,13 @@ describe("MyToken", () => {
 
     it("should have the correct decimals", async () => {
         expect(await myTokenC.decimals()).equal(18);
+    });
+
+    it("should return 0 totalSupply", async () => {
+        expect(await myTokenC.totalSupply()).equal(0);
+    });
+
+    it("should return 0 balance for signer 0", async () => {
+        expect(await myTokenC.balanceOf(signers[0]  .address)).equal(0);
     });
 });
