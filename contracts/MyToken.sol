a2c03298811d16503bbf78e44e358527aed375d6
awj1052 <awj1052@naver.com>
2025-04-11 18:30:59 +0900

    refactor: test

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -12,11 +12,11 @@ contract MyToken {
     // 변수는 storage에 저장됨. storage는 블록체인에 저장되는 데이터, memory는 임시로 저장되는 데이터
     // storage는 가스비가 비쌈, memory는 가스비가 적게 듦
     // 지역변수는 memory에 저장하는 것이 가장 효율적임
-    constructor(string memory _name, string memory _symbol, uint8 _decimals) {
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _amount) {
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
-        _mint(1*10**uint256(decimals), msg.sender); // msg.sender는 발행하는 사람 // 1 MT
+        _mint(_amount *10**uint256(decimals), msg.sender); // msg.sender는 발행하는 사람 // amount MT
     }
 
     function _mint(uint256 amount, address owner) internal {
--- test/MyToken.ts
+++ test/MyToken.ts
@@ -3,47 +3,53 @@ import { expect } from "chai";
 import { MyToken } from "../typechain-types";
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
-describe("MyToken", () => {
+const mintingAmount = 100n; // bigint
+const decimals = 18n;
+
+describe("My Token", () => {
     let myTokenC: MyToken;
     let signers: HardhatEthersSigner[];
 
     before(async () => {
         signers = await hre.ethers.getSigners();
-        myTokenC = await hre.ethers.deployContract("MyToken", ["MyToken", "MT", 18]);
+        myTokenC = await hre.ethers.deployContract("MyToken", ["MyToken", "MT", decimals, mintingAmount]);
     });
+    describe("Basic state value check", () => {
+        it("should have the correct name", async () => {
+            expect(await myTokenC.name()).equal("MyToken");
+        });
+    
+        it("should have the correct symbol", async () => {
+            expect(await myTokenC.symbol()).equal("MT");
+        });
+    
+        it("should have the correct decimals", async () => {
+            expect(await myTokenC.decimals()).equal(decimals);
+        });
+    
+        it("should return 100 totalSupply", async () => {
+            expect(await myTokenC.totalSupply()).equal(mintingAmount * 10n** decimals);
+        });
+    })
 
-    it("should have the correct name", async () => {
-        expect(await myTokenC.name()).equal("MyToken");
-    });
+    describe("Mint", () => {
+        // 1MT = 1e18
+        it("should return 1MT balance for signer 0", async () => {
+            const signer0 = signers[0];
+            expect(await myTokenC.balanceOf(signer0)).equal(mintingAmount * 10n ** decimals);
+        });
 
-    it("should have the correct symbol", async () => {
-        expect(await myTokenC.symbol()).equal("MT");
-    });
+        it("should have 0.5MT", async () => {
+            const signer1 = signers[1];
+            await myTokenC.transfer(hre.ethers.parseUnits("0.5", decimals), signer1.address);
+            expect(await myTokenC.balanceOf(signer1)).equal(hre.ethers.parseUnits("0.5", decimals));
+        });
 
-    it("should have the correct decimals", async () => {
-        expect(await myTokenC.decimals()).equal(18);
-    });
-
-    it("should return 0 totalSupply", async () => {
-        expect(await myTokenC.totalSupply()).equal(10n**18n);
-    });
-
-    // 1MT = 1e18
-    it("should return 1MT balance for signer 0", async () => {
-        const signer0 = signers[0];
-        expect(await myTokenC.balanceOf(signer0)).equal(1n*10n**18n);
-    });
-
-    it("should have 0.5MT", async () => {
-        const signer1 = signers[1];
-        await myTokenC.transfer(hre.ethers.parseUnits("0.5", 18), signer1.address);
-        expect(await myTokenC.balanceOf(signer1)).equal(hre.ethers.parseUnits("0.5", 18));
-    });
-
-    it("should be reverted with insufficient balance error", async () => {
-        const signer1 = signers[1];
-        await expect( // testing framework가 대신 실행(await)하고 오류를 확인
-            myTokenC.transfer(hre.ethers.parseUnits("1.1", 18), signer1.address) // 이 자체를 넘겨줌
-        ).to.be.revertedWith("insufficient balance")
+        it("should be reverted with insufficient balance error", async () => {
+            const signer1 = signers[1];
+            await expect( // testing framework가 대신 실행(await)하고 오류를 확인
+                myTokenC.transfer(hre.ethers.parseUnits((mintingAmount + 1n).toString(), decimals), signer1.address) // 이 자체를 넘겨줌
+            ).to.be.revertedWith("insufficient balance")
+        });
     });
 });
