821e45f33b4df27ff06cf2f5857132432914e736
awj1052 <awj1052@naver.com>
2025-05-12 01:57:20 +0900

    feat: NativeBank withdraw

--- contracts/NativeBank.sol
+++ contracts/NativeBank.sol
@@ -6,6 +6,16 @@ contract NativeBank {
     constructor() {
     }
 
+    function withdraw() external {
+        uint256 balance = balanceOf[msg.sender];
+        require(balance > 0, "insufficient balance");
+
+        (bool success,) = msg.sender.call{value: balance}("");
+        require(success, "failed to send native token");
+
+        balanceOf[msg.sender] = 0;
+    }
+
     receive() external payable {
         balanceOf[msg.sender] += msg.value;
     }
