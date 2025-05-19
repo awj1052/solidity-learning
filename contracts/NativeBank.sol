53504356ccb2cfe0685f7adc996204b93c65aff8
awj1052 <awj1052@naver.com>
2025-05-20 02:50:29 +0900

    exploit: NativeBank

--- contracts/NativeBank.sol
+++ contracts/NativeBank.sol
@@ -10,13 +10,14 @@ contract NativeBank {
         uint256 balance = balanceOf[msg.sender];
         require(balance > 0, "insufficient balance");
 
-        (bool success,) = msg.sender.call{value: balance}("");
+        // 0으로 만들기 전에 call때문에 재진입 공격이 발생할 수 있다. re-entrancy 공격
+        (bool success,) = msg.sender.call{value: balance}(""); // msg.sender의 receive, fallback을 호출
         require(success, "failed to send native token");
 
         balanceOf[msg.sender] = 0;
     }
 
-    receive() external payable {
+    receive() external payable { // fallback으로 대체 가능
         balanceOf[msg.sender] += msg.value;
     }
 }
